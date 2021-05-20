4f40ea5fdd1a813140f43c62a75d0ee53c93074c
Magdalena <magdalena.horbaczyk@gmail.com>
2021-05-20 23:36:05 +0200

    initial commit

---
+++ customer-service/src/test/java/com/creditapp/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creditapp.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
